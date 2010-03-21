5c8ba3e71b84b1d2d1c534e86442f92a7fc20ac8
Justin_K <Justin_K@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-21 20:03:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-photo-fetcher/tags/1.1.11@220407 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Main.php
+++ Main.php
@@ -8,11 +8,28 @@
  * Plugin URI: http://www.justin-klein.com/projects/facebook-photo-fetcher
  */
 
+/*
+ * Copyright 2010 Justin Klein (email: justin@justin-klein.com)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 //The "magic tag" identifier
 global $fpf_name, $fpf_version, $fpf_identifier, $fpf_homepage;
 $fpf_name       = "Facebook Photo Fetcher";
-$fpf_version    = "1.1.11.2";
+$fpf_version    = "1.1.11.3";
 $fpf_identifier = "FBGallery";
 $fpf_homepage   = "http://www.justin-klein.com/projects/facebook-photo-fetcher";
 
